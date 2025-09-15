92afa9d0beee572f7910bbaeab5303b9925f3517
alanturker <alanturker@gmail.com>
2025-09-15 15:34:55 +0300

    first commit

---
+++ Habitify/HabitifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitifyApp.swift
+//  Habitify
+//
+//  Created by Turker Alan on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
